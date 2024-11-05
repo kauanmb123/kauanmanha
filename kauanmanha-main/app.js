191bde164cabe0cdd9a3465008559be616f68bc8
kauanmb123 <146395124+kauanmb123@users.noreply.github.com>
2024-11-05 11:04:33 -0300

    Add files via upload

---
+++ kauanmanha-main/app.js
@@ -0,0 +1,21 @@
+function criaCartao(categoria,pergunda,resposta)
+
+let container = document.getElementById('container')
+let cartao = documento.container('artigo')
+cartao.className = 'cartao'
+
+cartao.innerHTNL=
+<div class="cartao__pergunta">
+<h3>${categoria}</h3>
+<div class="cartao__conteudo__pergunta">
+    <p>${pergunta</p>
+</div>
+<div class="cartao__conteudo__pergunta">
+    <p>${resposta}</p>
+</div>
+</div>
+
+
+
+}
+ 
